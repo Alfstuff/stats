98946d632e00fb0059a378bc4c9924146c5f2555
Santo Commarato <cylynex@gmail.com>
2017-11-07 09:55:17 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+require_once("inc/header.php");
+?>
+
+<div id="page-wrapper">
+
+    <?php include ("inc/rewrite.php"); ?>
+
+</div>
+<!-- /#page-wrapper -->
+
+<?php require_once("inc/footer.php"); ?>
